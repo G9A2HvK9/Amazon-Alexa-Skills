f3167097b435ab5b77245c5aa876a92eb1a91e68
hendrik <g9a2hvk9@me.com>
2017-09-07 23:24:39 +0200

    Ties in number facts API

--- modules/2/fact_checking_application/server.rb
+++ modules/2/fact_checking_application/server.rb
@@ -5,12 +5,15 @@ require 'net/http'
 class Server < Sinatra::Base
 
   post '/' do
+
+    number_facts_uri = URI("http://numbersapi.com/42")
+    number_fact = Net::HTTP.get(number_facts_uri)
     {
       version: "1.0",
       response: {
         outputSpeech: {
             type: "PlainText",
-            text: "42 is... meh... ok"
+            text: number_fact
           }
       }
     }.to_json
