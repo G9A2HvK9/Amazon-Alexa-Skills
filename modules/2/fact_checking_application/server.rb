df6fdefe11836aabbd053d340eeb33082c4bf7b1
hendrik <g9a2hvk9@me.com>
2017-09-07 23:19:18 +0200

    sets up simple alexa skill

--- modules/2/fact_checking_application/server.rb
+++ modules/2/fact_checking_application/server.rb
@@ -1,22 +1,19 @@
-require 'sinatra'
+require 'Sinatra'
 require 'json'
 require 'net/http'
 
-post '/' do 
-  parsed_request = JSON.parse(request.body.read)
-  number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
-  fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
+class Server < Sinatra::Base
 
-  number_facts_uri = URI("http://numbersapi.com/#{ number }/#{ fact_type }")
-  number_fact = Net::HTTP.get(number_facts_uri)
+  post '/' do
+    {
+      version: "1.0",
+      response: {
+        outputSpeech: {
+            type: "PlainText",
+            text: "42 is... meh... ok"
+          }
+      }
+    }.to_json
+  end
 
-  { 
-    version: "1.0",
-    response: {
-      outputSpeech: {
-          type: "PlainText",
-          text: number_fact
-        }
-    }
-  }.to_json
 end
