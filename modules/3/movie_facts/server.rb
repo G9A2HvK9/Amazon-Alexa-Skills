ac49e96393ba89f3fc2a59efd47910bf64dff123
hendrik <g9a2hvk9@me.com>
2017-09-09 00:34:15 +0200

    Adds session persistence

--- modules/3/movie_facts/server.rb
+++ modules/3/movie_facts/server.rb
@@ -1,21 +1,79 @@
 require 'sinatra'
 require 'json'
+require 'imdb'
 
 class Server < Sinatra::Base
 
   post '/' do
     parsed_request = JSON.parse(request.body.read)
-    p parsed_request
+    this_is_the_first_question = parsed_request["session"]["new"] || parsed_request["session"]["attributes"].empty?
+    restart_requested = (parsed_request["request"]["intent"]["name"] == "AMAZON.StartOverIntent")
 
-    return{
-      versio: "1.0",
-      response: {
-        outputSpeech: {
-          type: "PlainText",
-          text: "This is the first question"
+    p parsed_request["request"]["intent"]["name"] == "FollowUp"
+
+    if restart_requested
+      return{
+        version: "1.0",
+        sessionAttributes: {},
+          response: {
+            outputSpeech: {
+              type: "PlainText",
+              text: "Ok. Starting over. What movie would you like to know about?"
+            },
+          shouldEndSession: false
+          }
+        }.to_json
+    end
+
+    if this_is_the_first_question
+      requested_movie = parsed_request["request"]["intent"]["slots"]["Movie"]["value"]
+      movie_list = Imdb::Search.new(requested_movie).movies
+      movie = movie_list.first
+      return{
+        version: "1.0",
+        sessionAttributes: {
+          movieTitle: requested_movie
+        },
+          response: {
+            outputSpeech: {
+              type: "PlainText",
+              text: movie.plot_synopsis
+            }
+          }
+        }.to_json
+    end
+
+    if parsed_request["request"]["intent"]["name"] == "FollowUp"
+      movie_title = parsed_request["session"]["attributes"]["movieTitle"]
+      movie_list = Imdb::Search.new(movie_title).movies
+      movie = movie_list.first
+      role = parsed_request["request"]["intent"]["slots"]["Role"]["value"]
+      p role
+      p movie.director.join
+      p movie.cast_members.join(",")
+
+      if role == "directed"
+        response_text = "#{movie_title} was directed by #{movie.director.join}"
+      end
+
+      if role == "starred in"
+        response_text = "#{movie_title} starred #{movie.cast_members.join(",")}"
+      end
+
+
+      return{
+        version: "1.0",
+        sessionAttributes: {
+          movieTitle: movie_title
+        },
+        response: {
+          outputSpeech: {
+            type: "PlainText",
+            text: response_text
+          }
         }
-      }
-    }.to_json
+      }.to_json
+    end
   end
 
 
